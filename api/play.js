a98621d649c17b4c184cf25de845d4a116796d55
CACBMusic <CACBMusica@gmail.com>
2026-05-01 22:03:32 -0400

    Reduce token expiration time from 30 to 20 seconds

--- api/play.js
+++ api/play.js
@@ -10,7 +10,7 @@ export default function handler(req, res) {
     return res.status(400).json({ error: "Missing path" });
   }
 
-  const expires = Math.floor(Date.now() / 1000) + 30; // 2 minutos
+  const expires = Math.floor(Date.now() / 1000) + 20; // 20 seguntos
 
   const data = `${path}:${expires}`;
 
